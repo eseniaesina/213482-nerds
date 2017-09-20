f51af2c69e303593d19ee7df6b014c1136aa5bcd
eseniaesina <eseniae@gmail.com>
2017-09-20 13:46:49 +0300

    Правки+

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
-var link = document.querySelector(".button-contacts");
-			
-			var popup = document.querySelector(".modal-write-us");
-			var close = popup.querySelector(".modal-close");
-			
-			link.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.add("modal-show");
-			});
-			
-			close.addEventListener("click", function (evt) {
-				evt.preventDefault();
-				popup.classList.remove("modal-show");
-			});
+var link = document.querySelector(".button-contacts");			
+var popup = document.querySelector(".modal-write-us");
+var close = popup.querySelector(".modal-close");		
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal-show");
+});		
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.remove("modal-show");
+});
