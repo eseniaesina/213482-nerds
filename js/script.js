d8dc29eca125a1338791d8d91a7bf61ade94f21f
eseniaesina <eseniae@gmail.com>
2017-09-21 16:50:20 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var link = document.querySelector(".button-contacts");
 var popup = document.querySelector(".modal-write-us");
 var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
+var login = popup.querySelector("[name=login]");
 var mail = popup.querySelector("[name=mail]");
 var text = popup.querySelector("[name=text]");
 link.addEventListener("click", function (evt) {
@@ -12,19 +12,19 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
-  popup.classList.remove("modal-error");
+	popup.classList.remove("modal-error");
 });
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !mail.value || !text.value) {
-      evt.preventDefault();
-      console.log("Укажите данные");
-    }
-  });
+	if (!login.value || !mail.value || !text.value) {
+		evt.preventDefault();
+		popup.classList.add("modal-error");
+	}
+});
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
-        popup.classList.remove("modal-error");
-      }
-    }
-  });
+	if (evt.keyCode === 27) {
+		if (popup.classList.contains("modal-show")) {
+		popup.classList.remove("modal-show");
+		popup.classList.remove("modal-error");
+		}
+	}
+});
